d46f2ef7fdcecc486904ef482a07cfbe96d1ac32
dim2r <dim2r@yandex.ru>
2019-01-29 20:24:54 +0400

    1st

--- slitherRL.py
+++ slitherRL.py
@@ -23,3 +23,29 @@ class Board():
         self.cells  =  [[0 for x in range(dimension)] for y in range(dimension)]
         self.creatures =  [[0 for x in range(dimension)] for y in range(dimension)]
 
+
+
+def MAIN_PYGAME():
+    pygame.init()
+
+    windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+    pygame.display.set_caption('Hello world!')
+    windowSurface.fill(Color.WHITE)
+    basicFont = pygame.font.SysFont(None, 55)
+    text = basicFont.render('Hello world!!!!', True, Color.WHITE, Color.BLUE)
+    windowSurface.blit(text, (0,0))
+    pygame.draw.line(windowSurface, Color.BLUE, (60, 60), (120, 60), 4)
+    pygame.draw.line(windowSurface, Color.BLUE, (120, 60), (60, 120))
+    pygame.draw.line(windowSurface, Color.BLUE, (60, 120), (120, 120), 4)
+
+
+    pygame.display.update()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+if __name__ == "__main__":
+    MAIN_PYGAME()
